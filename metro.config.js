1108e2443396cfdcd6b760267e528704e0748c16
정서 <seojung@jungseo.local>
2025-09-22 01:20:33 +0900

    settings: watchman 등 초기세팅 및 빌드에러 해결

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,19 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watcher: {
+    // Watchman 비활성화
+    watchman: {
+      deferStates: [],
+    },
+    // 대신 Node.js watcher 사용
+    watchmanPath: null,
+  },
+  server: {
+    // 서버 설정
+    port: 8081,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
